5bd69f5c72f1b3d7f9e37d0749f54062e47f4990
smo202 <jerrylu202@gmail.com>
2017-07-17 21:48:12 -0400

    implement two-way scrolling

--- iTone/Controllers/PlaySoundViewController.swift
+++ iTone/Controllers/PlaySoundViewController.swift
@@ -12,9 +12,11 @@ class PlaySoundViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var playButton: UIButton!
     
-    let nib = UINib(nibName: "NoteCollectionViewCell", bundle: nil)
+    let nib = UINib(nibName: Constants.collectionViewCell.noteCollectionViewCell, bundle: nil)
     
     var arrayOfNoteCardItems: [NoteCardItem] = [
+        NoteCardItem(image: #imageLiteral(resourceName: "treble-c")),
+        NoteCardItem(image: #imageLiteral(resourceName: "treble-bsharp")),
         NoteCardItem(image: #imageLiteral(resourceName: "treble-c")),
         NoteCardItem(image: #imageLiteral(resourceName: "treble-csharp")),
         NoteCardItem(image: #imageLiteral(resourceName: "treble-d")),
@@ -30,12 +32,17 @@ class PlaySoundViewController: UIViewController, UIGestureRecognizerDelegate {
         NoteCardItem(image: #imageLiteral(resourceName: "treble-bsharp")),
         NoteCardItem(image: #imageLiteral(resourceName: "treble-c"))
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.register(nib, forCellWithReuseIdentifier: "NoteCollectionViewCell")
+        collectionView.register(nib, forCellWithReuseIdentifier: Constants.collectionViewCell.noteCollectionViewCell)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let start = CGPoint(x: CGFloat(2) * collectionView.bounds.size.width, y: collectionView.contentOffset.y)
+        collectionView.setContentOffset(start, animated: false)
     }
     
     @IBAction func playButtonPressed(_ sender: UIButton) {
@@ -44,28 +51,32 @@ class PlaySoundViewController: UIViewController, UIGestureRecognizerDelegate {
         case 0:
             print("Play C")
         case 1:
-            print("Play C Sharp")
+            print("Play B Sharp")
         case 2:
-            print("Play D")
+            print("Play C")
         case 3:
-            print("Play E Flat")
+            print("Play C Sharp")
         case 4:
-            print("Play E")
+            print("Play D")
         case 5:
-            print("Play F")
+            print("Play E Flat")
         case 6:
-            print("Play F Sharp")
+            print("Play E")
         case 7:
-            print("Play G")
+            print("Play F")
         case 8:
-            print("Play A Flat")
+            print("Play F Sharp")
         case 9:
-            print("Play A")
+            print("Play G")
         case 10:
-            print("Play B Flat")
+            print("Play A Flat")
         case 11:
-            print("Play B")
+            print("Play A")
         case 12:
+            print("Play B Flat")
+        case 13:
+            print("Play B")
+        case 14:
             print("Play B Sharp/C")
         default:
             break
@@ -75,22 +86,28 @@ class PlaySoundViewController: UIViewController, UIGestureRecognizerDelegate {
     func checkIfAtEndOfNotes(current: Int, target: Int) -> Bool {
         return current == target
     }
+    func checkIfAtBeginningOfNotes(current: Int, target: Int) -> Bool {
+        return current == target
+    }
     
-    func loopToBeginning() {
+    func loopToBeginningOrEnd() {
         let pageNumber = Int(collectionView.contentOffset.x / collectionView.frame.size.width)
-        let cgpoint = CGPoint(x: 0, y: collectionView.contentOffset.y)
+        let beginningCgpoint = CGPoint(x: CGFloat(2) * collectionView.bounds.size.width, y: collectionView.contentOffset.y)
+        let endingCGpoint = CGPoint(x: CGFloat(15) * collectionView.bounds.size.width - CGFloat(1), y: collectionView.contentOffset.y)
         if checkIfAtEndOfNotes(current: pageNumber, target: arrayOfNoteCardItems.count - 1) {
-            collectionView.setContentOffset(cgpoint, animated: false)
+            collectionView.setContentOffset(beginningCgpoint, animated: false)
+        } else if checkIfAtBeginningOfNotes(current: pageNumber, target: 1) {
+            collectionView.setContentOffset(endingCGpoint, animated: false)
         }
     }
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        loopToBeginning()
+        loopToBeginningOrEnd()
     }
 }
 
 extension PlaySoundViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NoteCollectionViewCell", for: indexPath) as! NoteCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.collectionViewCell.noteCollectionViewCell, for: indexPath) as! NoteCollectionViewCell
         let noteCardItem = arrayOfNoteCardItems[indexPath.section]
         cell.imageView.image = noteCardItem.image
         
---
+++ iTone/Supporting/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  iTone
+//
+//  Created by Jerry Lu on 7/17/17.
+//  Copyright © 2017 Jerry Lu. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct collectionViewCell {
+        static let noteCollectionViewCell = "NoteCollectionViewCell"
+    }
+}
