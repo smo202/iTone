0963853abd2f447ab732d7eca47645c097276149
smo202 <jerrylu202@gmail.com>
2017-07-24 19:04:51 -0400

    finished the scales practice section

--- iTone/Controllers/ScalesPracticeViewController.swift
+++ iTone/Controllers/ScalesPracticeViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ScalesPracticeViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var playButton: UIButton!
+    
+    var player: AVQueuePlayer?
     
     let nib = UINib(nibName: Constants.CollectionViewCell.scaleCollectionViewCell, bundle: nil)
     
@@ -39,6 +42,144 @@ class ScalesPracticeViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         let start = CGPoint(x: CGFloat(2) * collectionView.bounds.size.width, y: collectionView.contentOffset.y)
         collectionView.setContentOffset(start, animated: false)
+    }
+    
+}
+
+extension ScalesPracticeViewController {
+    func actuallyPlaySound(randomTonic: Int, note2: String, note3: String, note4: String, note5: String, note6: String, note7: String, note8: String) {
+        let url = Bundle.main.url(forResource: Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic], withExtension: "m4a")!
+        let url2 = Bundle.main.url(forResource: note2, withExtension: "m4a")!
+        let url3 = Bundle.main.url(forResource: note3, withExtension: "m4a")!
+        let url4 = Bundle.main.url(forResource: note4, withExtension: "m4a")!
+        let url5 = Bundle.main.url(forResource: note5, withExtension: "m4a")!
+        let url6 = Bundle.main.url(forResource: note6, withExtension: "m4a")!
+        let url7 = Bundle.main.url(forResource: note7, withExtension: "m4a")!
+        let url8 = Bundle.main.url(forResource: note8, withExtension: "m4a")!
+        
+        let tonicItem = AVPlayerItem(url: url)
+        let item2 = AVPlayerItem(url: url2)
+        let item3 = AVPlayerItem(url: url3)
+        let item4 = AVPlayerItem(url: url4)
+        let item5 = AVPlayerItem(url: url5)
+        let item6 = AVPlayerItem(url: url6)
+        let item7 = AVPlayerItem(url: url7)
+        let item8 = AVPlayerItem(url: url8)
+        
+        player = AVQueuePlayer(items: [tonicItem, item2, item3, item4, item5, item6, item7, item8])
+        player?.play()
+    }
+    
+    func playSound(scale: String) {
+        let randomTonic = Int(arc4random_uniform(12))
+        var note2: String
+        var note3: String
+        var note4: String
+        var note5: String
+        var note6: String
+        var note7: String
+        var note8: String
+        
+        switch scale {
+        case "Ionian":
+            note2 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 2]
+            note3 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 4]
+            note4 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 5]
+            note5 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 7]
+            note6 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 9]
+            note7 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 11]
+            note8 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 12]
+            actuallyPlaySound(randomTonic: randomTonic, note2: note2, note3: note3, note4: note4, note5: note5, note6: note6, note7: note7, note8: note8)
+        case "Aeolian":
+            note2 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 2]
+            note3 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 3]
+            note4 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 5]
+            note5 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 7]
+            note6 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 8]
+            note7 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 10]
+            note8 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 12]
+            actuallyPlaySound(randomTonic: randomTonic, note2: note2, note3: note3, note4: note4, note5: note5, note6: note6, note7: note7, note8: note8)
+        case "Harmonic Minor":
+            note2 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 2]
+            note3 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 3]
+            note4 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 5]
+            note5 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 7]
+            note6 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 8]
+            note7 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 11]
+            note8 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 12]
+            actuallyPlaySound(randomTonic: randomTonic, note2: note2, note3: note3, note4: note4, note5: note5, note6: note6, note7: note7, note8: note8)
+        case "Dorian":
+            note2 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 2]
+            note3 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 3]
+            note4 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 5]
+            note5 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 7]
+            note6 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 9]
+            note7 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 10]
+            note8 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 12]
+            actuallyPlaySound(randomTonic: randomTonic, note2: note2, note3: note3, note4: note4, note5: note5, note6: note6, note7: note7, note8: note8)
+        case "Phrygian":
+            note2 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 1]
+            note3 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 3]
+            note4 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 5]
+            note5 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 7]
+            note6 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 8]
+            note7 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 10]
+            note8 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 12]
+            actuallyPlaySound(randomTonic: randomTonic, note2: note2, note3: note3, note4: note4, note5: note5, note6: note6, note7: note7, note8: note8)
+        case "Lydian":
+            note2 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 2]
+            note3 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 4]
+            note4 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 6]
+            note5 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 7]
+            note6 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 9]
+            note7 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 11]
+            note8 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 12]
+            actuallyPlaySound(randomTonic: randomTonic, note2: note2, note3: note3, note4: note4, note5: note5, note6: note6, note7: note7, note8: note8)
+        case "Mixolydian":
+            note2 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 2]
+            note3 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 4]
+            note4 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 5]
+            note5 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 7]
+            note6 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 9]
+            note7 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 10]
+            note8 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 12]
+            actuallyPlaySound(randomTonic: randomTonic, note2: note2, note3: note3, note4: note4, note5: note5, note6: note6, note7: note7, note8: note8)
+        case "Locrian":
+            note2 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 1]
+            note3 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 3]
+            note4 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 5]
+            note5 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 6]
+            note6 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 8]
+            note7 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 10]
+            note8 = Constants.ArrangedPitches.arrayOfArrangedPitches[randomTonic + 12]
+            actuallyPlaySound(randomTonic: randomTonic, note2: note2, note3: note3, note4: note4, note5: note5, note6: note6, note7: note7, note8: note8)
+        default:
+            break
+        }
+    }
+    
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        let pageNumber = Int(collectionView.contentOffset.x / collectionView.frame.size.width)
+        switch pageNumber {
+        case 2:
+            playSound(scale: "Ionian")
+        case 3:
+            playSound(scale: "Aeolian")
+        case 4:
+            playSound(scale: "Harmonic Minor")
+        case 5:
+            playSound(scale: "Dorian")
+        case 6:
+            playSound(scale: "Phrygian")
+        case 7:
+            playSound(scale: "Lydian")
+        case 8:
+            playSound(scale: "Mixolydian")
+        case 9:
+            playSound(scale: "Locrian")
+        default:
+            break
+        }
     }
 }
 
--- iTone/Supporting/Constants.swift
+++ iTone/Supporting/Constants.swift
@@ -28,11 +28,64 @@ struct Constants {
         static let f = "f"
         static let g = "g"
         static let highC = "high-c"
+        static let highAFlat = "high-a-flat"
+        static let highA = "high-a"
+        static let highBFlat = "high-b-flat"
+        static let highB = "high-b"
+        static let highCSharp = "high-c-sharp"
+        static let highD = "high-d"
+        static let highEFlat = "high-e-flat"
+        static let highE = "high-e"
+        static let highFSharp = "high-f-sharp"
+        static let highF = "high-f"
+        static let highG = "high-g"
+        static let shortMiddleC = "short-middle-c"
+        static let shortHighC = "short-high-c"
+        static let shortAFlat = "short-a-flat"
+        static let shortA = "short-a"
+        static let shortBFlat = "short-b-flat"
+        static let shortB = "short-b"
+        static let shortCSharp = "short-c-sharp"
+        static let shortD = "short-d"
+        static let shortEFlat = "short-e-flat"
+        static let shortE = "short-e"
+        static let shortFSharp = "short-f-sharp"
+        static let shortF = "short-f"
+        static let shortG = "short-g"
     }
     
     struct Segues {
         static let correctAnswer = "correctAnswerSegue"
         static let wrongAnswer = "wrongAnswerSegue"
         static let toApp = "toAppSegue"
+    }
+    
+    struct ArrangedPitches {
+        static let arrayOfArrangedPitches = [
+            Constants.Pitches.shortMiddleC,
+            Constants.Pitches.shortCSharp,
+            Constants.Pitches.shortD,
+            Constants.Pitches.shortEFlat,
+            Constants.Pitches.shortE,
+            Constants.Pitches.shortF,
+            Constants.Pitches.shortFSharp,
+            Constants.Pitches.shortG,
+            Constants.Pitches.shortAFlat,
+            Constants.Pitches.shortA,
+            Constants.Pitches.shortBFlat,
+            Constants.Pitches.shortB,
+            Constants.Pitches.shortHighC,
+            Constants.Pitches.highCSharp,
+            Constants.Pitches.highD,
+            Constants.Pitches.highEFlat,
+            Constants.Pitches.highE,
+            Constants.Pitches.highF,
+            Constants.Pitches.highFSharp,
+            Constants.Pitches.highG,
+            Constants.Pitches.highAFlat,
+            Constants.Pitches.highA,
+            Constants.Pitches.highBFlat,
+            Constants.Pitches.highB
+        ]
     }
 }
