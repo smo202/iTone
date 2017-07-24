94937d391431b681b8bfb12340f039196ec12ccc
smo202 <jerrylu202@gmail.com>
2017-07-24 11:52:41 -0400

    added looping scroll to scales practice

--- iTone/Controllers/ScalesPracticeViewController.swift
+++ iTone/Controllers/ScalesPracticeViewController.swift
@@ -16,13 +16,16 @@ class ScalesPracticeViewController: UIViewController {
     
     let arrayOfScaleCardItems: [ScaleCardItem] = [
         ScaleCardItem(scaleName: "Ionian Mode/Major Scale", scaleFormat: "W, W, H, W, W, W, H"),
+        ScaleCardItem(scaleName: "Locrian Mode", scaleFormat: "H, W, W, H, W, W, W"),
+        ScaleCardItem(scaleName: "Ionian Mode/Major Scale", scaleFormat: "W, W, H, W, W, W, H"),
         ScaleCardItem(scaleName: "Aeolian Mode/Natural Minor Scale", scaleFormat: "W, H, W, W, H, W, W"),
         ScaleCardItem(scaleName: "Harmonic Minor Scale", scaleFormat: "W, H, W, W, H, WH, H"),
         ScaleCardItem(scaleName: "Dorian Mode", scaleFormat: "W, H, W, W, W, H, W"),
         ScaleCardItem(scaleName: "Phrygian Mode", scaleFormat: "H, W, W, W, H, W, W"),
         ScaleCardItem(scaleName: "Lydian Mode", scaleFormat: "W, W, W, H, W, W, H"),
         ScaleCardItem(scaleName: "Mixolydian Mode", scaleFormat: "W, W, H, W, W, H, W"),
-        ScaleCardItem(scaleName: "Locrian Mode", scaleFormat: "H, W, W, H, W, W, W")
+        ScaleCardItem(scaleName: "Locrian Mode", scaleFormat: "H, W, W, H, W, W, W"),
+        ScaleCardItem(scaleName: "Ionian Mode/Major Scale", scaleFormat: "W, W, H, W, W, W, H")
     ]
     
     override func viewDidLoad() {
@@ -31,6 +34,34 @@ class ScalesPracticeViewController: UIViewController {
         collectionView.register(nib, forCellWithReuseIdentifier: Constants.CollectionViewCell.scaleCollectionViewCell)
         playButton.layer.cornerRadius = 10
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let start = CGPoint(x: CGFloat(2) * collectionView.bounds.size.width, y: collectionView.contentOffset.y)
+        collectionView.setContentOffset(start, animated: false)
+    }
+}
+
+extension ScalesPracticeViewController {
+    func checkIfAtEndOfNotes(current: Int, target: Int) -> Bool {
+        return current == target
+    }
+    func checkIfAtBeginningOfNotes(current: Int, target: Int) -> Bool {
+        return current == target
+    }
+    
+    func loopToBeginningOrEnd() {
+        let pageNumber = Int(collectionView.contentOffset.x / collectionView.frame.size.width)
+        let beginningCgpoint = CGPoint(x: CGFloat(2) * collectionView.bounds.size.width, y: collectionView.contentOffset.y)
+        let endingCGpoint = CGPoint(x: CGFloat(10) * collectionView.bounds.size.width - CGFloat(1), y: collectionView.contentOffset.y)
+        if checkIfAtEndOfNotes(current: pageNumber, target: arrayOfScaleCardItems.count - 1) {
+            collectionView.setContentOffset(beginningCgpoint, animated: false)
+        } else if checkIfAtBeginningOfNotes(current: pageNumber, target: 1) {
+            collectionView.setContentOffset(endingCGpoint, animated: false)
+        }
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        loopToBeginningOrEnd()
     }
 }
 
